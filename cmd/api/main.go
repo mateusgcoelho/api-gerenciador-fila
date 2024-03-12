9977761e2a86c2ea62b1ab7b22e73221c8a6b96e
João Mateus <mateusgamescoelho547@gmail.com>
2024-03-12 20:01:20 -0300

    feat: Inicializar servidor

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/joho/godotenv"
+	"github.com/mateusgcoelho/api-gerenciador-fila/internal/router"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Erro ao carregar variaveis de ambiente.")
+	}
+
+	router.Initialize()
 }
---
+++ internal/router/router.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Initialize() {
+	r := gin.Default()
+
+	serverPort := fmt.Sprintf("%s", os.Getenv("SERVER_PORT"))
+
+	if err := r.Run(serverPort); err != nil {
+		log.Fatal("Não foi possível iniciar o serviço.")
+	}
+}
